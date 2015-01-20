ef376a40d15f0e3206675e38594173cdfef4d6dd
Oscar99 <od_cortez@galileo.edu>
2015-01-20 12:27:40 -0600

    Add Testing Function

--- spec/spec.js
+++ spec/spec.js
@@ -3,5 +3,23 @@ describe("First jasmine project", function() {
 
     expect(helloWorld()).toBe("hello world");
   });
+ 
+  it ("should write the title on the page",function(){
+  	expect( textTitle).toBe("Testing Jasmine");
+  	expect(textTitle).not.toBe(" ");
+
+});
+it ("sould include in the title Jasmine",function(){
+	expect(textTitle).toMatch(/Jasmine/i);
+	
+	});
+it("verificacion de suma",function(){
+	expect(suma(2,2)).toBe(4);
+	expect(suma(3,6)).toBe(9);
+	expect(suma(-50,1000)).toBe(950);
+	expect(suma(-50,"1000")).toBe(950);
+	expect(suma (20,40)).toBeGreaterThan(30);
+	expect(suma (40,40)).toBeLessThan(81);
+	});
 
 });
--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
+var textTitle = "Testing Jasmine";
 function helloWorld() {
   return "hello world";
 }
+
+function titulo  (){
+	 document.getElementById("titleDiv").innerHTML= textTitle;
+};
+titulo();
+
+
+function suma (n1, n2){
+	if(isNaN(n1)|| isNaN(n2)){
+		return null;
+
+	}else{
+		n1 = parseInt(n1);
+		n2 = parseInt(n2);
+		return n1 + n2 ;
+
+	};
+};
+suma();
